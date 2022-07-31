0d140a25d11b1dca05cebcda0f740b9e6497d415
aniketarchit2400 <110337369+aniketarchit2400@users.noreply.github.com>
2022-07-31 05:34:51 -0700

    Add files via upload

---
+++ JS/main.js
@@ -0,0 +1,16 @@
+$(document).ready(function(){
+  $nav=$('.nav');
+  $toggleCollapse=$('.toggle-collapse');
+  $toggleCollapse.click(function(){
+    $nav.toggleClass('collapse');
+
+  })
+  $('.owl-carousel').owlCarousel({
+      loop:true,
+      autoplay: true,
+      autoplayTimeout:3000,
+      dots:false,
+      nav:true
+  });
+
+});
